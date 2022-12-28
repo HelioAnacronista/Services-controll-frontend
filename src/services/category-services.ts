2f56b8db06421813bfa247bcffbc99d07b12b61d
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-12-28 16:29:08 -0300

    test category

--- src/services/category-services.ts
+++ src/services/category-services.ts
@@ -4,22 +4,23 @@ import { BASE_URL } from '../utils/system';
 import { CategoryDTO } from '../models/category';
 import { requestBackend } from '../utils/requests';
 
+const xhr = new XMLHttpRequest();
+
 export function findPageRequest(page: number, name : string, size = 10 ,sort = "id") {
-   const config : AxiosRequestConfig = {
-       method : "GET",
-       baseURL: BASE_URL,
-       url: "category",
-       params: {
-          page,
-          name,
-          size,
-          sort
-       }
-   }
-
-
-   return axios(config);
-}
+   
+   const url = `${BASE_URL}/category?page=${page}&name=${name}&size=${size}&sort=${sort}`;
+ 
+   xhr.open('GET', url);
+   xhr.onload = function () {
+     if (xhr.status === 200) {
+       console.log(xhr.responseText);
+     } else {
+       console.log('Error: ' + xhr.status);
+     }
+   };
+   xhr.send();
+ }
+ 
 
 export function findById(id: number) {
    return axios.get(`${BASE_URL}category/${id}`);
@@ -27,6 +28,7 @@ export function findById(id: number) {
 
 export function deleteById(id: number) {
    const config : AxiosRequestConfig = {
+
       method: "DELETE",
       url: `category/${id}`,
    }
