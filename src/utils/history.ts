66eb73727243eb3815b777f5cd1b57658d7f433b
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2023-01-10 17:11:49 -0300

    fix: history 6.4++

--- src/utils/history.ts
+++ src/utils/history.ts
@@ -1,5 +1,5 @@
-import { createBrowserHistory } from 'history';
-
-
+import { createBrowserHistory } from "@remix-run/router";
 //instaciando um obj do tipo broserhistory
-export const history = createBrowserHistory();
+export const history = createBrowserHistory({v5Compat: true });
+
+
